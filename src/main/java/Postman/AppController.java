38315aadaf687987b60b705d2d97aeca8ea0e604
Nameiggy <164057591+Nameiggy@users.noreply.github.com>
2024-12-21 15:54:09 +0300

    Gresenko

---
+++ src/main/java/Postman/AppController.java
@@ -0,0 +1,83 @@
+package Postman;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+@RestController
+@RequestMapping("/app/v1")
+public class AppController {
+
+
+    @GetMapping("/getRequest")
+    public ResponseEntity<?> handleGetRequest(
+            @RequestParam("id") int id,
+            @RequestParam("name") String name)
+            throws IOException,InterruptedException  {
+
+            // Проверка условий
+            if (id <= 10) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Недопустимый идентификатор: должно быть больше 10");
+            }
+            if (name.length() <= 4) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Недопустимое имя: длина должна быть больше 5");
+            }
+                // Задержка в зависимости от значения id
+            if (id > 10 && id < 50) {
+                Thread.sleep(1000); // 1000 мс
+            } else {
+                Thread.sleep(500); // 500 мс
+            }
+                // Чтение содержимого файла getRequest.txt
+            String getRequest = new String(Files.readAllBytes(Paths.get("src/main/resources/getRequest.txt")));
+            String response = getRequest.replace("{name}", name);
+            return ResponseEntity.ok(response);
+
+
+    }
+    @PostMapping("/postRequest")
+    public ResponseEntity<String> handlePostRequest(
+            @RequestBody RequestBodyData requestBody)
+            throws IOException {
+             // Проверка условий
+            if (requestBody.getName() == null || requestBody.getName().isEmpty() ||
+                requestBody.getSurname() == null || requestBody.getSurname().isEmpty() ||
+                requestBody.getAge() == null || requestBody.getAge() == 0) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Отсутствуют обязательные поля: имя, фамилия или возраст");
+            }
+            String postAnswer = new String(Files.readAllBytes(
+                    Paths.get("src/main/resources/postRequest.txt")));
+            String response = postAnswer
+                .replace("{name}", requestBody.getName())
+                .replace("{surname}", requestBody.getSurname())
+                .replace("{age}", String.valueOf(requestBody.getAge()));
+
+        return ResponseEntity.ok(response);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/Postman/RequestBodyData.java
@@ -0,0 +1,25 @@
+package Postman;
+
+public class RequestBodyData {
+    private String name;
+    private String surname;
+    private Integer age;
+
+   public RequestBodyData(){
+
+   }
+    public String getName() {
+        return name;
+    }
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+
+    public Integer getAge() {
+        return age;
+    }
+
+}
